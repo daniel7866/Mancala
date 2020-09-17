a813687cbe64c16ac4c609862f0b6b0de6b5dc81
daniel7866 <daniel_7866@yahoo.com>
2020-09-17 14:28:44 +0300

    bug fix

--- mancala.pl
+++ mancala.pl
@@ -2,11 +2,13 @@
 %turn(player) = who can play now
 :- dynamic pocket/3.
 :- dynamic turn/1.
+:- dynamic winner/1.
 
 
 cleanUp:-
   retractall(pocket(_,_,_)),
-  retractall(turn(_)).
+  retractall(turn(_)),
+  retractall(winner(_)).
 start:-
   start(5),
   assert(pocket(bank,human,0)),
@@ -20,12 +22,10 @@ start(Pos):-
   NextPos is Pos-1,
   start(NextPos).
 
-%Game has ended if one side has no stones left
-ended:-
-  isEmptyRow(5,human);
-  isEmptyRow(5,cpu).
+isEmptyRow(BoardSide):-
+  isEmptyRow(5,BoardSide).
 
-isEmptyRow(-1).
+isEmptyRow(-1,_).
 isEmptyRow(Pos,BoardSide):-
   pocket(Pos,BoardSide,0),
   NextPos is Pos-1,
@@ -110,3 +110,31 @@ switchTurns:-
   turn(cpu),
   retract(turn(cpu)),
   assert(turn(human)).
+
+%collect all stones from the entire row to the bank
+collectRow(BoardSide):-
+  collectRow(5,BoardSide).%overloading
+collectRow(-1,_).%finished the entire row
+collectRow(Pos,BoardSide):-
+  emptyCurrPocket(Pos,BoardSide,NumOfStones),
+  pocket(bank,BoardSide,BankNumOfStones),
+  UpBankNumOfStones is NumOfStones + BankNumOfStones,
+  retract(pocket(bank,BoardSide,BankNumOfStones)),
+  assert(pocket(bank,BoardSide,UpBankNumOfStones)),
+  NewPos is Pos-1,
+  collectRow(NewPos,BoardSide).
+
+  %Game has ended if one side has no stones left
+gameEnded:-
+  gameEnded1,%one row is empty and other row collected
+  pocket(bank,human,HumanNumOfStones),
+  pocket(bank,cpu,CpuNumOfStones),
+  ((HumanNumOfStones>CpuNumOfStones,assert(winner(human)));
+  (HumanNumOfStones<CpuNumOfStones,assert(winner(cpu)));
+  (HumanNumOfStones = CpuNumOfStones,assert(winner(tie)))).%no one won - it's a tie
+gameEnded1:-%check if a row is empty - collect the other
+  isEmptyRow(human),
+  collectRow(cpu).
+gameEnded1:-%check if a row is empty - collect the other
+  isEmptyRow(cpu),
+  collectRow(human).
